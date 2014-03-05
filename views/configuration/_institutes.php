88c6ae0e258040ccd027892d6e8c6dc3d8b3491b
Thomas Hackl <thomas.hackl@uni-passau.de>
2014-03-05 11:28:50 +0100

    config form improved

--- views/configuration/_institutes.php
+++ views/configuration/_institutes.php
@@ -1,4 +1,4 @@
-    <label class="caption" for="studycourse[]">
+    <label class="caption" for="institutes[]">
         <?= _('An welche Einrichtungen (außer der/den eigenen) soll die gewählte Einrichtung Nachrichten schreiben dürfen?') ?>
     </label>
     <ul id="faculties">
@@ -14,7 +14,7 @@
         </li>
                 <?php } ?>
         <li class="faculty">
-            <input type="checkbox" name="institutes[]" class="faculty_select" value="<?= $i['Institut_id'] ?>"/>
+            <input type="checkbox" name="institutes[]" class="faculty_select" value="<?= $i['Institut_id'] ?>"<?= ($config['institutes'][$i['Institut_id']]) ? ' checked="checked"' : '' ?>/>
             <label for="<?= $i['Institut_id'] ?>">
                 <?= Assets::img('icons/16/blue/institute.png') ?>
                 <?= htmlReady($i['Name']) ?>
@@ -31,7 +31,8 @@
             <ul id="institutes_<?= $i['Institut_id'] ?>">
             <?php } else { ?>
                 <li class="institute">
-                    <input type="checkbox" class="subtree" name="institutes[]" value="<?= $i['Institut_id'] ?>"<?= ($config['institutes'][$i['Institut_id']]) ? ' checked="checked"' : '' ?> data-faculty-id="<?php $o=new Institute($i['Institut_id']);echo $o->fakultaets_id ?>"/>
+                    <?php $o=new Institute($i['Institut_id']); ?>
+                    <input type="checkbox" class="subtree" name="institutes[]" value="<?= $i['Institut_id'] ?>"<?= ($config['institutes'][$o->fakultaets_id] ? ' checked="checked" disabled="disabled"' : ($config['institutes'][$i['Institut_id']] ? ' checked="checked"' : '')) ?> data-faculty-id="<?= $o->fakultaets_id ?>"/>
                     <?= htmlReady($i['Name']) ?>
                 </li>
             <?php } ?>
