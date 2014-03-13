d45e77efbc0fd0865162237253a734361e556633
Thomas Hackl <thomas.hackl@uni-passau.de>
2014-03-13 11:43:57 +0100

    correct settings

--- GarudaCronjob.php
+++ GarudaCronjob.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * GarudaCronjob.class.php
- * 
+ *
  * Creates and executes cron jobs for sending messages.
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of
@@ -30,18 +30,11 @@ class GarudaCronjob extends CronJob {
     }
 
     public static function getParameters() {
-        return array(
-            'verbose' => array(
-                'type' => 'boolean',
-                'default' => false,
-                'status' => 'optional',
-                'description' => dgettext('garudaplugin', 'Sollen Ausgaben erzeugt werden'),
-            ),
-        );
+        return array();
     }
 
     public function setUp() {
-        
+
     }
 
     /**
@@ -50,23 +43,19 @@ class GarudaCronjob extends CronJob {
     public function execute($last_result, $parameters = array()) {
         Log::set('garuda', $GLOBALS['TMP_PATH'].'/garuda.log');
         $jobs = GarudaModel::getCronEntries();
+        $m = new Message();
         foreach ($jobs as $job) {
             // Mark current entry as locked.
             if (GarudaModel::lockCronEntry($job['job_id'])) {
                 $sender = new User($job['sender_id']);
                 // Get recipients.
                 $users = array_map(function($u) {
-                    return new User($u);
+                    $o = new User($u);
+                    return $o->username;
                 }, json_decode($job['recipients']));
                 $numRec = sizeof($users);
-                // Mail forwarding needs E-Mail address instead of user_id.
-                $mailRec = array_map(function($u) {
-                    $user = new User($u);
-                    return $user->email;
-                }, array_unique($users));
                 // Send Stud.IP message.
-                $m = new Message();
-                $message_id = $m->send($sender->user_id, $users, $job['subject'], $job['message']);
+                $message = $m->send($sender->user_id, $users, $job['subject'], $job['message']);
                 // Build full name of the sender for log.
                 $senderName = $sender->vorname.' '.$sender->nachname;
                 if ($sender->title_front) {
@@ -76,16 +65,16 @@ class GarudaCronjob extends CronJob {
                     $senderName .= ', '.$sender->title_rear;
                 }
                 // Write status to log file.
-                if ($message_id) {
-                    Log::info_garuda(sprintf('Message from %s to %s recipients was sent:\n%s\n\n%s', $senderName, $numRec, $job['subject'], $job['message']));
+                if ($message) {
+                    Log::info_garuda(sprintf("Message from %s to %s recipients was sent:\n%s\n\n%s", $senderName, $numRec, $job['subject'], $job['message']));
                 } else {
-                    Log::error_garuda(sprintf('Message from %s to %s recipients could not be sent:\n%s\n\n%s', $senderName, $numRec, $job['subject'], $job['message']));
+                    Log::error_garuda(sprintf("Message from %s to %s recipients could not be sent:\n%s\n\n%s", $senderName, $numRec, $job['subject'], $job['message']));
                 }
             }
         }
     }
 
     public function tearDown() {
-        
+
     }
 }
