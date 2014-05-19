26e31d6cace296f7be2acd829a504061614dccff
Thomas Hackl <thomas.hackl@uni-passau.de>
2014-05-19 09:24:03 +0200

    refine migration

--- migrations/02_cronjobs_done.php
+++ migrations/02_cronjobs_done.php
@@ -2,7 +2,8 @@
 class CronjobsDone extends DBMigration
 {
     function up(){
-        DBManager::get()->exec("ALTER TABLE `garuda_messages` ADD `done` TINYINT AFTER `locked`");
+        DBManager::get()->exec("ALTER TABLE `garuda_messages` ADD `done` TINYINT NOT NULL DEFAULT 0 AFTER `locked`");
+        DBManager::get()->exec("UPDATE `garuda_messages` SET `done`=1 WHERE `locked`=1");
     }
 
     function down()
