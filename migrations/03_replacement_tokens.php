6abe67edd969602883728d25a3cacca274e5a42d
Thomas Hackl <thomas.hackl@uni-passau.de>
2014-06-18 16:13:08 +0200

    correct SQL

--- migrations/03_replacement_tokens.php
+++ migrations/03_replacement_tokens.php
@@ -5,8 +5,8 @@ class ReplacementTokens extends DBMigration
         DBManager::get()->exec("CREATE TABLE `garuda_tokens` (
             `token_id` INT NOT NULL AUTO_INCREMENT,
             `job_id` VARCHAR(32) NOT NULL REFERENCES `garuda_messages`.`job_id`,
-            `user_id` VARCHAR(32) REFERENCES `auth_user_md5`.`user_id` DEFAULT NULL,
-            `token` VARCHAR(255) NOT NULL DEFAULT '',
+            `user_id` VARCHAR(32) NOT NULL REFERENCES `auth_user_md5`.`user_id`,
+            `token` VARCHAR(1000) NOT NULL DEFAULT '',
             `mkdate` INT NOT NULL DEFAULT 0,
             PRIMARY KEY (`token_id`),
             INDEX `job` (`job_id`)
