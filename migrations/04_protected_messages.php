c43bb3d011a38397012fc9e14991f9fdcbd51779
Thomas Hackl <thomas.hackl@uni-passau.de>
2014-06-24 13:47:11 +0200

    correct migration SQL

--- migrations/04_protected_messages.php
+++ migrations/04_protected_messages.php
@@ -2,8 +2,7 @@
 class ProtectedMessages extends DBMigration
 {
     function up(){
-        DBManager::get()->exec("ALTER TABLE `garuda_messages`
-            ADD `protected` TINYINT(1) AFTER `done` NOT NULL DEFAULT 0");
+        DBManager::get()->exec("ALTER TABLE `garuda_messages` ADD `protected` TINYINT(1) NOT NULL DEFAULT 0 AFTER `done`");
     }
 
     function down()
