1133ff6beb15ad2b4662d7f56b014d5b88a2f7c8
hackl <thomas.hackl@uni-passau.de>
2016-06-17 12:57:40 +0200

    use default form class

--- views/userfilter/addrestricted.php
+++ views/userfilter/addrestricted.php
@@ -1,7 +1,7 @@
 <?php use Studip\Button, Studip\LinkButton; ?>
-<form class="studip_form" id="filterform" action="<?= $controller->url_for('userfilter/save') ?>" method="post">
+<form class="default" id="filterform" action="<?= $controller->url_for('userfilter/save') ?>" method="post">
     <h2><?= dgettext('garudaplugin', 'Welche Personen sollen erfasst werden?') ?></h2>
-    <div id="filterfields">
+    <section id="filterfields">
         <?php foreach ($filterfields as $className => $data) { ?>
         <div class="filterfield" id="<?= $className ?>" data-relation="<?= htmlReady($data['relation']) ?>">
             <label class="caption" for="field[]">
@@ -13,17 +13,19 @@
             </span>
         </div>
         <?php } ?>
-    </div>
-    <br/>
-    <?php foreach ($flash->flash as $key => $value) { ?>
-        <?php if (is_array($value)) { ?>
-            <?php foreach ($value as $entry) { ?>
-    <input type="hidden" name="<?= htmlReady($key) ?>[]" value="<?= htmlReady($entry) ?>"/>
-            <?php } ?>
-        <?php } else { ?>
-    <input type="hidden" name="<?= htmlReady($key) ?>" value="<?= htmlReady($value) ?>"/>
-        <?php } ?>
-    <?php } ?>
+    </section>
+    <br>
+    <section>
+        <?php foreach ($flash->flash as $key => $value) : ?>
+            <?php if (is_array($value)) : ?>
+                <?php foreach ($value as $entry) : ?>
+                    <input type="hidden" name="<?= htmlReady($key) ?>[]" value="<?= htmlReady($entry) ?>"/>
+                <?php endforeach ?>
+            <?php else : ?>
+                <input type="hidden" name="<?= htmlReady($key) ?>" value="<?= htmlReady($value) ?>"/>
+            <?php endif ?>
+        <?php endforeach ?>
+    </section>
     <?= CSRFProtection::tokenTag() ?>
     <?= Button::createAccept(dgettext('garudaplugin', 'Filter übernehmen'), 'submit', array('data-dialog-button' => '')) ?>
     <?= LinkButton::createCancel(dgettext('garudaplugin', 'Abbrechen'), $controller->url_for('message'), array('data-dialog-button' => '', 'data-dialog' => 'close')) ?>
