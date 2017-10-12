f3e7f56d628d540b8194f78b8f81691cc90a9e17
hackl <thomas.hackl@uni-passau.de>
2017-10-12 14:13:11 +0200

    UTF-8 encoding in translation files

--- extractTranslationStrings.sh
+++ extractTranslationStrings.sh
@@ -11,9 +11,7 @@ TRANSLATIONFILES_RELATIVE_PATHS="."
 for language in en
 do
     test -f "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/garudaplugin.po" && mv "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/garudaplugin.po" "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/garudaplugin.po.old"
-    > "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/garudaplugin.UTF-8.po"
-    find $TRANSLATIONFILES_RELATIVE_PATHS \( -iname "*.php" \) | xargs xgettext --from-code=ISO-8859-1 -j -n --language=PHP -o "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/garudaplugin.UTF-8.po"
-    msgconv --to-code=iso-8859-1 "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/garudaplugin.UTF-8.po" -o "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/garudaplugin.po"
+    > "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/garudaplugin.po"
+    find $TRANSLATIONFILES_RELATIVE_PATHS \( -iname "*.php" \) | xargs xgettext --from-code=UTF-8 -j -n --language=PHP -o "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/garudaplugin.po"
     test -f "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/garudaplugin.po.old" && msgmerge "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/garudaplugin.po.old" "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/garudaplugin.po" --output-file="$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/garudaplugin.po"
-    test -f "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/garudaplugin.UTF-8.po" && rm "$LOCALE_RELATIVE_PATH/$language/LC_MESSAGES/garudaplugin.UTF-8.po"
 done
